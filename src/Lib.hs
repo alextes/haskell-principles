e232364be03b14ab89cd0c64c7506f11cf73ef7c
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2019-01-09 00:02:24 +0100

    Rewrite magic

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,12 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+tensDigit :: Integral a => a -> a
+tensDigit x = d
+  where
+    xLast = x `div` 10
+    d = xLast `mod` 10
+
+tensDigit' :: Integral a => a -> a
+tensDigit' x = snd $ divMod xLast 10
+  where
+    xLast = x `div` 10
