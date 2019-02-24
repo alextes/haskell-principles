c2169de6e2dc578b5fe6d6b44b12f672037856a3
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2019-02-24 20:03:37 +0100

    commit more exercises

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,5 +1,7 @@
 module Lib where
 
+import Data.List (nub)
+
 tensDigit :: Integral a => a -> a
 tensDigit x = d
   where
@@ -12,9 +14,28 @@ tensDigit' x = snd $ divMod xLast 10
     xLast = x `div` 10
 
 foldBool :: a -> a -> Bool -> a
-foldBool a b c = if c then a else b
+foldBool a b c =
+  if c
+    then a
+    else b
 
--- foldBool' :: a -> a -> Bool -> a
--- foldBool' a b c
---   | _ _ True = a
---   | _ _ False = b
+data BinaryTree a
+  = Leaf
+  | Node (BinaryTree a)
+         a
+         (BinaryTree a)
+  deriving (Eq, Ord, Show)
+
+tree = Node Leaf 1 Leaf
+
+mapBTree :: (a -> b) -> BinaryTree a -> BinaryTree b
+mapBTree f Leaf = Leaf
+mapBTree f (Node aTree val bTree) =
+  Node (mapBTree f aTree) (f val) (mapBTree f bTree)
+
+foldBTree :: (a -> b -> b) -> b -> BinaryTree a -> b
+foldBTree _ acc Leaf = acc
+foldBTree f b (Node left a right) = f a bOfLeftRight
+  where
+    bOfLeftRight = foldBTree f bOfLeft right
+    bOfLeft = foldBTree f b left
